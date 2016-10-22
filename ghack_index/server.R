ab9146941b294b022ad5129ff53b7554156aef3d
hagbardssub <alex.mueller@lessannoyingcrm.com>
2016-10-22 09:08:56 -0500

    automatic addition of new entries to the base list of homeless people

--- ghack_index/server.R
+++ ghack_index/server.R
@@ -1,14 +1,20 @@
 library(shiny)
 
-#setwd("C:/Users/Alexander/Documents/r sandox/globalhack6/ghack_index")
+
+
+#setwd("C:/Users/Alexander/Documents/GitHub/homefulness/ghack_index")
 data=read.csv(paste0("test.csv"))
 
 # Define server logic required to plot various variables against mpg
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
-  
+  clone_data_row<-function(dataFrame){
+    return(rbind(dataFrame,dataFrame[sample(1:nrow(dataFrame),1),]))
+  } 
   
   output$barPlot <- renderPlot({
+    invalidateLater(10, session)
+    data<<-clone_data_row(data)
     barplot(
       table(data[[input$variable]])
     )
