f06719668be8dd96574bd7cf3c825e2d43d0710a
hagbardssub <alex.mueller@lessannoyingcrm.com>
2016-10-21 21:18:06 -0500

    added tabs for multiple visualizations

--- ghack_index/server.R
+++ ghack_index/server.R
@@ -1,10 +1,12 @@
 library(shiny)
 
-setwd("C:/Users/Alexander/Documents/r sandox/globalhack6/ghack_index")
+#setwd("C:/Users/Alexander/Documents/r sandox/globalhack6/ghack_index")
 data=read.csv(paste0("test.csv"))
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
+  
+  
   
   output$barPlot <- renderPlot({
     barplot(
--- ghack_index/ui.R
+++ ghack_index/ui.R
@@ -1,20 +1,22 @@
 library(shiny)
 
 # Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("People"),
-  sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("White" = "White",
-                     "Black" = "Black"))
-  ),
-  
-  
-  
-  # Show the caption and plot of the requested variable against mpg
-  mainPanel(
-    plotOutput("barPlot")
-  )
-))
+shinyUI(
+  navbarPage("My Application",
+    tabPanel("Race Bar Plot",
+      pageWithSidebar(
+        headerPanel("People"),
+        sidebarPanel(
+          selectInput("variable", "Variable:",
+                      list("White" = "White",
+                           "Black" = "Black"))
+        ),
+        mainPanel(
+          plotOutput("barPlot")
+        )
+      )
+    ),
+    tabPanel("YourViz1"),
+    tabPanel("YourViz2")
+  )  
+)
