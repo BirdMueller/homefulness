23c23150e5410e163b2719d25b0d314f3ea456e4
bhoai <becky.hoai@gmail.com>
2016-10-22 20:11:42 -0500

    radio selection pie chart
    mmm pie

--- ghack_index_becky/ui.R
+++ ghack_index_becky/ui.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(DT)
+library(leaflet)
 
 # Define UI for miles per gallon application
 shinyUI(
@@ -9,30 +11,77 @@ shinyUI(
         sidebarPanel(
           selectInput("variable", "Variable:",
                       list("White" = "White",
-                           "Black" = "Black"))
+                           "Black" = "Black")),
+          selectInput("updatePeople", "Auto-Update:",
+                      list("No" = "No",
+                           "Yes" = "Yes"))
         ),
         mainPanel(
           plotOutput("barPlot")
         )
       )
     ),
-    tabPanel("Disability Status", plotOutput("piePlot")),	# end tabPanel
-    tabPanel("Histograms", 
-	 pageWithSidebar(
-        headerPanel("Numerics To Plot"),
+    tabPanel("Table With Scores",
+      pageWithSidebar(
+        headerPanel("People"),
         sidebarPanel(
-          selectInput("histVar", "Info to Plot:",
-                      list("Months Homeless" = "MonthsHomelessPastThreeYears",
-                           "Days on Street" = "DaysOnStreet"))
+          selectInput("variable", "Variable:",
+            list("White" = "White",
+            "Black" = "Black")),
+          selectInput("updateTable", "Auto-Update:",
+                      list("No" = "No",
+                           "Yes" = "Yes"))
         ),
         mainPanel(
-          plotOutput("histPlot")
-        ) # end mainPanel
-      ) # end pageWithSidebar
-	), # end tabPanel
-	tabPanel("Heatmap", 
-          plotOutput("heatPlot")
-	) # end tabPanel
-
+          dataTableOutput("table")
+        )
+      )    
+    ),
+    tabPanel("A Map",
+      pageWithSidebar(
+        headerPanel("Map"),
+        sidebarPanel(
+         selectInput("variable", "Variable:",
+                     list("White" = "White",
+                          "Black" = "Black")),
+         selectInput("updateMap", "Auto-Update:",
+                     list("No" = "No",
+                          "Yes" = "Yes"))
+        ),
+        mainPanel(
+          plotOutput("mymap")
+        )
+      )
+    ),
+    ###begin Becky's visualizations
+    tabPanel("Demographics", pageWithSidebar(
+      headerPanel("Demographic Data"),
+      sidebarPanel(
+        radioButtons("demChoice", label = h3("Select one:"),
+                     choices = list("Race" = 1, "Disability Status" = 2, "Veteran Status" = 3), 
+                     selected = 2)
+      ),
+      mainPanel(
+        plotOutput("piePlot")
+      ) # end mainPanel
+    ) # end pageWithSidebar
+    ), # end tabPanel
+    tabPanel("Histograms", 
+             pageWithSidebar(
+               headerPanel("Numerics To Plot"),
+               sidebarPanel(
+                 selectInput("histVar", "Info to Plot:",
+                             list("Months Homeless" = "MonthsHomelessPastThreeYears",
+                                  "Days on Street" = "DaysOnStreet"))
+               ),
+               mainPanel(
+                 plotOutput("histPlot")
+               ) # end mainPanel
+             ) # end pageWithSidebar
+    ), # end tabPanel
+    tabPanel("Heatmap", 
+             plotOutput("heatPlot")
+    ) # end tabPanel
+    ###end Becky's visualizations
   )  
 )
